9073c39cb6842ba09a2fe55176a06e4cb05dce89
Jayanth Koushik <jnkoushik@gmail.com>
2017-06-16 23:25:30 -0400

    Add gel implementation

---
+++ gel/gel.py
@@ -0,0 +1,194 @@
+"""gel.py: main package file; group elastic net implementation.
+
+This implementation uses accelerated proximal gradient descent to
+solve the optimization problem:
+
+    min_b (1/2m)||y - b_0 - sum_j A_j@b_j||^2 +
+          sum_j {sqrt{n_j}(l_1*||b_j|| + l_2*||b_j||^2)}
+
+Here, and everywhere else, the "@" symbol is used to denote matrix
+multiplication. Denoting the first part of the objective by g,
+and the second part by h, the algorithm can be written as
+
+    v = b^{k-1} + ((k-2)/(k-1))*(b^{k-1} - b^{k-2})
+    b^k = prox_{t_k}(v - t_k*grad(g(v)))
+
+Here t_k is a step length chosen by backtracking line search,
+and prox is the proximal operator.
+
+The functions in this module use certain variables and conventions.
+These will be defined next.
+
+The coefficients are represented using a scalar bias b_0 and a matrix
+B where each row corresponds to a single group (with appropriate 0 padding).
+The root of the group sizes are stored in a vector sns. The features are
+stored in a 3D tensor A of size #groups x max{n_j} x m.
+
+a_1 = l_1*sns (broadcasted to the shape of B)
+a_2 = 2*l_2*sns (also broadcasted to the shape of B)
+"""
+
+import torch
+import numpy as np
+
+
+def _prox(B, a_1, a_2, t):
+    """Compute the prox operator of h.
+
+    In this case, this reduces to a simple soft thresholding:
+
+        prox_t(x)_0 = x_0
+        prox_t(x)_j =
+            if ||x_j|| > t*l_1*sqrt(n_j):
+                (x_j/||x_j||)*((||x_j|| - t*l_1*sqrt(n_j)) /
+                               (1 + 2*t*l_2*sqrt(n_j)))
+            else:
+                0
+
+    Here x_j denotes the coefficient corresponding to the j^th group.
+
+    Arguments have usual meanings. b_0 is not
+    passed to the function since it is not changed.
+    """
+    # First compute assuming 'if' part of the condition
+    ta_1 = t*a_1
+    norms = B.norm(p=2, dim=1).expand_as(B)
+    shrinkage = (norms - ta_1) / (1 + t*a_2)
+    prox = B * shrinkage / norms
+
+    # Threshold to zero if ||x_j|| is below threshold
+    thresh_cond = norms <= ta_1
+    prox[thresh_cond] = 0
+    return prox
+
+
+def _r(A, b_0, B, y):
+    """Compute y - b_0 - sum_j A_j@b_j."""
+    # bmm will return a tensor with individual A_j@b_j products
+    AB = torch.bmm(A.permute(0, 2, 1), B.unsqueeze(2)).sum(dim=0).squeeze()
+    return y - b_0 - AB
+
+
+def _g(A, b_0, B, y, m):
+    """Compute g(b)."""
+    # m is the number of samples
+    r = _r(A, b_0, B, y)
+    return r@r / (2.*m)
+
+
+def _grad(A, b_0, B, y, p, m):
+    """Compute the gradient of g."""
+    r = _r(A, b_0, B, y)
+    grad_b_0 = r.sum() / -m
+    # r must be broadcasted for the next operation
+    r = r.unsqueeze(0).unsqueeze(2).expand(p, m, 1)
+    grad_B = torch.bmm(A, r).squeeze() / -m
+    return grad_b_0, grad_B
+
+
+def gel_solve(As, y, l_1, l_2, t_init=None, ls_beta=None, b_init=None,
+              use_gpu=False, max_iters=None, rel_tol=1e-6):
+    """Solve a group elastic net problem.
+
+    Arguments:
+        As: list of feature matrices, one per group (size m x n_j).
+        y: vector of predictions.
+        l_1: the 2-norm coefficient.
+        l_2: the squared 2-norm coefficient.
+        t_init: initial step size to start line search; if None, it's set to
+            the value from the previous iteration.
+        ls_beta: shrinkage factor for line search; if None, no line search is
+            performed, and t_init is used as the step size for every iteration.
+        b_init: tuple (b_0, B) to initialize b.
+        use_gpu: whether or not to move tensors to GPU.
+        max_iters: maximum number of iterations; if None, there is no limit.
+        rel_tol: tolerance for exit criterion.
+    """
+    # First compute needed variables
+    p = len(As)
+    m = As[0].size()[0]
+    ns = torch.LongTensor([A_j.size()[1] for A_j in As])
+    if use_gpu:
+        ns = ns.cuda()
+    max_n_j = ns.max()
+
+    # Form the A matrix as needed
+    A = torch.zeros(p, max_n_j, m)
+    if use_gpu:
+        A = A.cuda()
+    for j in range(p):
+        A_j = As[j]
+        n_j = ns[j]
+        A[j, :n_j, :] = A_j.transpose(1, 0)
+
+    # Intialize b
+    if b_init is not None:
+        b_0, B = b_init
+    else:
+        b_0, B = 0., torch.zeros(p, max_n_j)
+    if use_gpu:
+        B = B.cuda()
+
+    # Compute a_1, a_2
+    sns = ns.float().sqrt().unsqueeze(1)
+    a_1 = (l_1*sns).expand_as(B)
+    a_2 = (2*l_2*sns).expand_as(B)
+
+    # Start the optimization
+    b_0_prev, B_prev = b_0, B
+    k = 1 # Iteration number
+    t = 1 # Initial step length (used if t_init is None)
+    while True:
+        # Compute the v terms
+        mom = (k - 2) / (k + 1.)
+        v_0 = b_0 + mom*(b_0 - b_0_prev)
+        V = B + mom*(B - B_prev)
+        g_v = _g(A, v_0, V, y, m)
+        grad_v_0, grad_V = _grad(A, v_0, V, y, p, m)
+
+        b_0_prev, B_prev = b_0, B
+
+        # Adjust the step size with backtracking line search
+        if t_init is not None:
+            t = t_init
+        while True:
+            # Compute the update based on gradient, then apply prox
+            b_0 = v_0 - t*grad_v_0
+            B = V - t*grad_V
+            B = _prox(B, a_1, a_2, t)
+
+            if ls_beta is None:
+                # Don't perform line search
+                break
+
+            # The line search condition is to exit when
+            #   g(b) <= g(v) + grad_v.T@(b - v) + (1/2t)||b - v||^2
+            g_b = _g(A, b_0, B, y, m)
+            b0_v0_diff = b_0 - v_0
+            B_V_diff = B - V
+            # grad_v.T@(b - v):
+            c_2 = grad_v_0*b0_v0_diff + (grad_V*B_V_diff).sum()
+            # (1/2t)||b - v||^2:
+            c_3 = (b0_v0_diff**2 + (B_V_diff**2).sum()) / (2.*t)
+
+            if g_b <= g_v + c_2 + c_3:
+                break
+            else:
+                t *= ls_beta
+
+        # Check max iterations exit criterion
+        if max_iters is not None and k == max_iters:
+            break
+        k += 1
+
+        # Check tolerance exit criterion
+        # Break if the relative change in 2-norm between b and
+        # b_prev is less than tol
+        b_0_diff = b_0 - b_0_prev
+        B_diff = B - B_prev
+        delta_norm = np.sqrt(b_0_diff**2 + (B_diff**2).sum())
+        b_norm = np.sqrt(b_0**2 + (B**2).sum())
+        if delta_norm < rel_tol * b_norm:
+            break
+
+    return b_0, B
---
+++ gel/tests/test_gel.py
@@ -0,0 +1,110 @@
+"""test_gel.py: framework to test the gel module."""
+
+import unittest
+
+import torch
+import cvxpy as cvx
+import numpy as np
+
+from gel.gel import gel_solve
+
+
+def gel_solve_cvx(As, y, l_1, l_2):
+    """Solve a group elastic net problem with cvx.
+
+    Arguments have the same meaning as in gel_solve.
+    """
+    # Convert torch tensors to numpy arrays
+    As = [As_j.numpy() for As_j in As]
+    y = y.numpy()
+
+    # Create the b variables
+    b_0 = cvx.Variable()
+    bs = []
+    ns = []
+    for A_j in As:
+        n_j = A_j.shape[1]
+        ns.append(n_j)
+        bs.append(cvx.Variable(n_j))
+
+    # Form g(b)
+    Ab = sum(A_j*b_j for A_j, b_j in zip(As, bs))
+    m = As[0].shape[0]
+    g_b = cvx.square(cvx.norm2(y - b_0 - Ab)) / (2*m)
+
+    # Form h(b)
+    h_b = sum(np.sqrt(n_j)*(l_1*cvx.norm2(b_j) + l_2*cvx.square(cvx.norm2(b_j)))
+              for n_j, b_j in zip(ns, bs))
+
+    # Build the optimization problem
+    obj = cvx.Minimize(g_b + h_b)
+    problem = cvx.Problem(obj, constraints=None)
+
+    problem.solve(solver="CVXOPT")
+
+    b_0 = b_0.value
+    # Form B as returned by gel_solve
+    p = len(As)
+    B = torch.zeros(p, max(ns))
+    for j in range(p):
+        b_j = np.asarray(bs[j].value)
+        if ns[j] == 1:
+            b_j = b_j.reshape(1,)
+        B[j, :ns[j]] = torch.FloatTensor(b_j)
+
+    return b_0, B
+
+
+class TestGel(unittest.TestCase):
+
+    """Test functions for gel_solve."""
+
+    def test_gel_birthwt(self):
+        """Test with the birth weight data."""
+        # Load data
+        X = np.loadtxt("data/birthwt/X.csv", skiprows=1, delimiter=",")
+        y = np.loadtxt("data/birthwt/y.csv", skiprows=1)
+        groups = [[0, 1, 2], [3, 4, 5], [6, 7], [8], [9, 10], [11], [12],
+                  [13, 14, 15]]
+
+        # Declare parameters
+        p = len(groups)
+        m = 189
+        l_1 = 4. / (2*m)
+        l_2 = 0.
+        t_init = 0.1
+        ls_beta = 0.9
+
+        # Covert to gel format
+        As = []
+        for j in range(p):
+            A_j = X[:, groups[j]]
+            As.append(torch.FloatTensor(A_j))
+        yt = torch.FloatTensor(y)
+
+        # Solve with both methods
+        for method in ["self", "cvx"]:
+            if method == "self":
+                b_0, B = gel_solve(As, yt, l_1, l_2, t_init, ls_beta,
+                                   max_iters=1000, rel_tol=0)
+            else:
+                b_0, B = gel_solve_cvx(As, yt, l_1, l_2)
+
+            # Convert B to a vector
+            b = np.zeros(X.shape[1])
+            for j in range(p):
+                b[groups[j]] = B[j, :len(groups[j])].numpy()
+
+            # Compute objective
+            r = y - b_0 - X@b
+            g_b = r@r / (2.*m)
+            b_j_norms = [np.linalg.norm(b[groups[j]], ord=2) for j in range(p)]
+            h_b = l_1 * sum(b_j_norms[j]*np.sqrt(len(groups[j]))
+                            for j in range(p))
+            if method == "self":
+                self_obj = g_b + h_b
+            else:
+                cvx_obj = g_b + h_b
+
+        # Compare self to ground truth
+        self.assertTrue(np.allclose(self_obj, cvx_obj))
