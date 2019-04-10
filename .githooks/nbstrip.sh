77dc899ae2d03ef499565c3cd15bbb03f263458c
Jayanth Koushik <jnkoushik@gmail.com>
2019-04-09 23:30:27 -0400

    Fix nbstrip hook

--- .githooks/nbstrip.sh
+++ .githooks/nbstrip.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-jq -cM --unbuffered '
-   (.cells[] | select(has("execution_count")) | .execution_count) = null
-  | .metadata = {"language_info": {"name": "python3", "pygments_lexer": "ipython3"}}
-  | .cells[].metadata = {}
-' $1 > $1.tmp && mv $1.tmp $1
+for nb in "$@"; do
+    jq -cM '
+       (.cells[] | select(has("execution_count")) | .execution_count) = null
+      | .metadata = {"language_info": {"name": "python3", "pygments_lexer": "ipython3"}}
+      | .cells[].metadata = {}
+    ' $nb > $nb.tmp && mv $nb.tmp $nb
+done
